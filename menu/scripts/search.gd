bfe913d0775413083dc24410a7b252e58301b4a7
Habs <brenohabib2013@gmail.com>
2024-05-25 17:08:11 -0300

    barra de pesquisa agora ordena em ordem alfabética após finalizar a pesquisa

--- menu/scripts/search.gd
+++ menu/scripts/search.gd
@@ -1,6 +1,12 @@
 extends Control
 
 @onready var vbox = $Tech/Buttons
+var original_button_order = []
+
+func _ready():
+	# Armazena a ordem original dos botões
+	for i in range(vbox.get_child_count()):
+		original_button_order.append(vbox.get_child(i))
 
 func _on_searchbar_text_changed(text):
 	var lower_text = text.to_lower()
@@ -18,13 +24,19 @@ func _on_searchbar_text_changed(text):
 
 	var matches = begins_with_matches + contains_matches
 
+	# Reordena os botões com base nos resultados da pesquisa
 	for i in range(matches.size()):
 		vbox.move_child(matches[i], i)
 
+	# Exibe/Oculta botões de acordo com o texto da pesquisa e estado original
 	for button in vbox.get_children():
 		if button in matches:
 			button.show()
 		elif text == "":
-			button.show()
+			# Se a pesquisa estiver vazia, restaura a ordem original
+			var original_index = original_button_order.find(button)
+			if original_index != -1:
+				vbox.move_child(button, original_index)
+			button.show()  # Exibe todos os botões quando a pesquisa estiver vazia
 		else:
 			button.hide()
